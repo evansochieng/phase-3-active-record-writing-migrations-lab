be2824687e90eeb6a0bdf72f8f93766506f937a8
Evans Ochieng <evansochieng121@gmail.com>
2022-11-01 16:31:39 +0300

    practice writing migrations

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    #create a table with name column as string
+    create_table :students do |t|
+      t.string :name
+    end
   end
 end
